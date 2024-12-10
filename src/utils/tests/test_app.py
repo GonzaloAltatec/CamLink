d46fc572d63dfaa4305e7cb20afea5ca26f8031c
GonzaloAltatec <gonzalorodriguez@altatec-seguridad.com>
2024-12-10 17:19:58 +0100

    Temporal test_app removing

--- src/utils/tests/test_app.py
+++ src/utils/tests/test_app.py
@@ -1,11 +1,11 @@
-from fastapi.testclient import TestClient
-from ...main import app
-
-client = TestClient(app)
-
-
-# Testing root endpoint response
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"details": "API Online"}
+# from fastapi.testclient import TestClient
+# from ...main import app
+#
+# client = TestClient(app)
+#
+#
+## Testing root endpoint response
+# def test_root():
+#    response = client.get("/")
+#    assert response.status_code == 200
+#    assert response.json() == {"details": "API Online"}
