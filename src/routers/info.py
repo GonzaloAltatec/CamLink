a1e7552f82f340ca83fdbe15cd9352d9101b0f91
GonzaloAltatec <gonzalorodriguez@altatec-seguridad.com>
2025-02-13 15:38:54 +0100

    feat: close #46

--- src/routers/info.py
+++ src/routers/info.py
@@ -1,4 +1,5 @@
 # Local libreries import
+from src.utils.db.schemas import IDList
 from src.utils.odoo import Odoo
 from src.utils.operations import Hikvision as Hik
 
@@ -38,47 +39,52 @@ async def fw_info(id: int):
 
 
 # Read NVR Data
-@router.get("/nvr", status_code=status.HTTP_200_OK)
-async def nvr_info(id: int):
-    device_list = client.sys_nvr(id)
+@router.post("/nvr", status_code=status.HTTP_200_OK)
+async def nvr_info(id_list: IDList):
     device_info_list = []
+    for id in id_list.ids:
+        device_list = client.sys_nvr(id)
 
-    if device_list is not None and isinstance(device_list, list):
-        for device in device_list:
-            api = Hik(device["host"], device["password"])
-            req = api.info()
-            if req is not None and isinstance(req, str):
-                root = ET.ElementTree(ET.fromstring(req))
+        if device_list is not None and isinstance(device_list, list):
+            for device in device_list:
+                api = Hik(device["host"], device["password"])
+                req = api.info()
+                if req is not None and isinstance(req, str):
+                    root = ET.ElementTree(ET.fromstring(req))
 
-                name_tag = root.find(
-                    "ns:deviceName", {"ns": "http://www.hikvision.com/ver20/XMLSchema"}
-                )
-                name_text = name_tag.text if name_tag is not None else None
+                    name_tag = root.find(
+                        "ns:deviceName",
+                        {"ns": "http://www.hikvision.com/ver20/XMLSchema"},
+                    )
+                    name_text = name_tag.text if name_tag is not None else None
 
-                firmware_tag = root.find(
-                    "ns:firmwareVersion",
-                    {"ns": "http://www.hikvision.com/ver20/XMLSchema"},
-                )
-                firmware_text = firmware_tag.text if firmware_tag is not None else None
+                    firmware_tag = root.find(
+                        "ns:firmwareVersion",
+                        {"ns": "http://www.hikvision.com/ver20/XMLSchema"},
+                    )
+                    firmware_text = (
+                        firmware_tag.text if firmware_tag is not None else None
+                    )
 
-                build_tag = root.find(
-                    "ns:firmwareReleasedDate",
-                    {"ns": "http://www.hikvision.com/ver20/XMLSchema"},
-                )
-                build_text = build_tag.text if build_tag is not None else None
+                    build_tag = root.find(
+                        "ns:firmwareReleasedDate",
+                        {"ns": "http://www.hikvision.com/ver20/XMLSchema"},
+                    )
+                    build_text = build_tag.text if build_tag is not None else None
 
-                model_tag = root.find(
-                    "ns:model", {"ns": "http://www.hikvision.com/ver20/XMLSchema"}
-                )
-                model_text = model_tag.text if model_tag is not None else None
+                    model_tag = root.find(
+                        "ns:model", {"ns": "http://www.hikvision.com/ver20/XMLSchema"}
+                    )
+                    model_text = model_tag.text if model_tag is not None else None
 
-                device_dict = {
-                    "device": name_text,
-                    "data": {
-                        "firmware": f"{firmware_text} {build_text}",
-                        "model": model_text,
-                    },
-                }
+                    device_dict = {
+                        "system": id,
+                        "device": name_text,
+                        "data": {
+                            "firmware": f"{firmware_text} {build_text}",
+                            "model": model_text,
+                        },
+                    }
 
-                device_info_list.append(device_dict)
+                    device_info_list.append(device_dict)
     return device_info_list
